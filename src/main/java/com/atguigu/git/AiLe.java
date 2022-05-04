535805fcad9989def8052b78f685edb54b5df1e7
wangyong <1923207092@qq.com>
2022-05-04 18:54:20 +0800

    git 3 commit

--- src/main/java/com/atguigu/git/AiLe.java
+++ src/main/java/com/atguigu/git/AiLe.java
@@ -4,5 +4,6 @@ public class AiLe  {
     public static void main(String[] args) {
         System.out.println("你好...........");
         System.out.println("aaaaaaaaaaaaaaaa");
+        System.out.println("git3");
     }
 }
